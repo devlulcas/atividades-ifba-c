cb5aa2a4d6f49e42b0ad01a4fcb3530d83575f7f
devlulcas <lucasalvesrego.contato@gmail.com>
2021-08-20 18:08:09 -0300

    modificaçao: adiçao do terceiro enunciado

--- l3/q3.c
+++ l3/q3.c
@@ -1,3 +1,13 @@
+/*
+  Desenvolva um programa em C que lê as 5 notas de um aluno da disciplina de Estrutura de Dados
+(ED) do curso técnico de Informática do IFBA - Campus Barreiras devendo calcular a média. O
+programa deve imprimir a média e verificar se a média desse aluno(a) é maior ou igual a sete, se for
+o programa deve emitir a seguinte mensagem: “Aluno(a) Aprovado na disciplina de ED!”. Se a média
+é menor que sete deve emitir a mensagem “Aluno(a) reprovado na disciplina ED!”. Se a média está
+entre 4 (maior ou igual) e 7 (menor que) o sistema deve emitir a mensagem “Aluno(a) em prova final
+da disciplina ED!”.
+*/
+
 #include <stdio.h>
 
 int main()
