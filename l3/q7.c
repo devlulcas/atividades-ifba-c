3e08e5a096d8eb4adca8c49c0170fd11a1d5ba66
devlulcas <lucasalvesrego.contato@gmail.com>
2021-08-20 18:16:58 -0300

    modificaçao: adiçao do setimo enunciado

--- l3/q7.c
+++ l3/q7.c
@@ -1,3 +1,12 @@
+/*
+  Desenvolva um programa em C que auxilie o IFBA - Campus Barreiras em relação a quantidade de
+alunos nos cursos técnicos de Informática e Edificações. O programa deve informar qual curso possui
+mais alunos e qual a quantidade total de alunos. Além disso, caso os cursos tenham a mesma
+quantidade de alunos, o programa deve emitir uma mensagem informando que ambos os cursos têm
+a mesma quantidade e apresentar essa quantidade. Por fim, caso algum curso não tenha nenhum
+aluno matriculado o programa deve informar que não existe alunos matriculados no curso.
+*/
+
 #include <stdio.h>
 
 int main()
@@ -25,6 +34,13 @@ int main()
     printf("Todos possuem a mesma quantidade de estudantes");
     return 0;
   }
+  
+  /*
+  | 0 && 0  = 0 |
+  | 0 && 1  = 0 |
+  | 1 && 1  = 1 |
+  | 1 && 0  = 0 | 
+  */
 
   if (edificacoes > informatica && edificacoes > alimentos) {
     printf("Edificações é o curso com mais estudantes, possuindo %d estudantes", edificacoes); 
