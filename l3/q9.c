262b5f550b8ab923b582a57e80a45ca041768636
devlulcas <lucasalvesrego.contato@gmail.com>
2021-08-20 18:18:51 -0300

    modificaçao: adiçao do nono enunciado

--- l3/q9.c
+++ l3/q9.c
@@ -1,3 +1,11 @@
+/*
+  O professor da disciplina de Estrutura de Dados do Curso Técnico de Informática do IFBA - Campus
+Barreiras solicitou aos alunos do curso técnico de Informática do terceiro ano que fizesse um
+programa na Linguagem C para auxiliar os alunos de uma escola de nível fundamental para verificar
+se um número é divisível por 5. O programa deve ler um número de um usuário e verificar se é divisível
+por 5.
+*/
+
 #include <stdio.h>
 
 int main()
