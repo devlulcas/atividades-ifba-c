da54cb27c46b19f65ba8699b59853e5cb5a9a713
devlulcas <lucasalvesrego.contato@gmail.com>
2021-08-23 23:42:22 -0300

    adiçao: questao nove respondida

---
+++ l4/q9.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    printf("\tIntituto Federal da Bahia - IFBA - Campus Barreiras.\n");
+    printf ("\tDisciplina: Estrutura de Dados - Turma 732.\n");
+    printf("\tProfessor: Djalma A. Lima Filho\n");
+    printf("\tAluno(a): Lucas Alves Rego \n\n\n");
+    
+    int bandeira;
+    float bandeira1, bandeira2, custo, km;
+    bandeira1 = 1.8;
+    bandeira2 = 2.3;
+    
+    printf("QUILOMETROS VIAJADOS:\n");
+    scanf("%f", &km);
+    printf("BANDEIRA UTILIZADA:\n");
+    scanf("%d", &bandeira);
+    
+    if (bandeira == 1)
+    {
+        custo = bandeira1 * km;
+    } 
+    else if (bandeira == 2)
+    {
+        custo = bandeira2 * km;    
+    }
+  
+    printf("CUSTO FINAL: %.2f", custo);
+    return(0);
+}
